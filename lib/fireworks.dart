25e6b6facc6932ca12a8ce642482b85bbf219694
Emily Fortuna <efortuna@google.com>
2019-07-03 15:10:05 -0700

    added customizable child to animate around and height/width limits

--- lib/fireworks.dart
+++ lib/fireworks.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
 
 import 'dart:math';
+import 'dart:async';
 
 import 'package:pimp_my_button/pimp_my_button.dart';
 
 /// A simple animation simulating fireworks over your entire device screen.
 class Fireworks extends StatelessWidget {
-  Fireworks({this.numberOfExplosions: 2, this.delay: 3});
+  Fireworks(
+      {this.numberOfExplosions: 2,
+      this.delay: 3,
+      this.child,
+      this.maxHeight,
+      this.maxWidth});
 
   /// The number of fireworks that are going on at any given time.
   final int numberOfExplosions;
 
   /// The longest length of time between one explosion and the next
-  /// of a particular firework. A new firework will explode on screen between 
+  /// of a particular firework. A new firework will explode on screen between
   /// 0 and `delay` seconds after the previous one finishes.
   final int delay;
 
+  /// The child that will be appearing across the screen. If nothing is
+  /// specified, by default a fireworks-like particle effect, from the
+  /// "pimp my button" package is used.
+  final Widget child;
+
+  /// Provides the maximum height bounds within which this Fireworks effect
+  /// should occur. If not specified, this falls back to the screen height.
+  final double maxHeight;
+
+  /// Provides the maximum width bounds within which this Fireworks effect
+  /// should occur. If not specified, this falls back to the screen width.
+  final double maxWidth;
+
   @override
   Widget build(BuildContext context) {
+    var mediaQueryData = MediaQuery.of(context);
+    var width = maxWidth ?? mediaQueryData.size.shortestSide;
+    var height = maxHeight ?? mediaQueryData.size.longestSide;
     return Stack(
-        children:
-            List.generate(numberOfExplosions, (i) => _OneFirework(delay)));
+        children: List.generate(numberOfExplosions,
+            (i) => _OneFirework(delay, height, width, child)));
   }
 }
 
 class _OneFirework extends StatefulWidget {
-  _OneFirework(this.delay);
+  _OneFirework(this.delay, this.width, this.height, this.child);
   final int delay;
+  final double width;
+  final double height;
+  final Widget child;
   @override
   _OneFireworkState createState() => _OneFireworkState();
 }
@@ -40,15 +65,9 @@ class _OneFireworkState extends State<_OneFirework> {
     Future.delayed(
         Duration(seconds: d),
         () => controller.forward(from: 0).then((_) async {
-              final mediaQueryData = MediaQuery.of(context);
-
               setState(() {
-                _x = random
-                    .nextInt(mediaQueryData.size.shortestSide.toInt())
-                    .toDouble();
-                _y = random
-                    .nextInt(mediaQueryData.size.longestSide.toInt())
-                    .toDouble();
+                _x = random.nextInt(widget.width.toInt()).toDouble();
+                _y = random.nextInt(widget.height.toInt()).toDouble();
               });
               await Future.delayed(Duration(seconds: 1));
               _makeFirework(controller);
@@ -59,15 +78,16 @@ class _OneFireworkState extends State<_OneFirework> {
   Widget build(BuildContext context) {
     return Positioned(
       child: PimpedButton(
-        particle: DemoParticle(),
+        particle: widget.child == null ? null : DemoParticle(),
         pimpedWidgetBuilder: (context, controller) {
           _makeFirework(controller);
-          return FloatingActionButton(
-            mini: true,
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            onPressed: () {},
-          );
+          return widget.child ??
+              FloatingActionButton(
+                mini: true,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                onPressed: () {},
+              );
         },
       ),
       left: _x,
